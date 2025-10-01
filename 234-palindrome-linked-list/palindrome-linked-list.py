bab304e87dcd1110c782b14b1ad5440026edcb8e
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-01 17:04:19 +0530

    Time: 47 ms (21.77%) | Memory: 34.8 MB (63.23%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -21,34 +21,59 @@
 #         return left >= right
 
 # approach 2: slow and fast
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         slow = head
+#         fast = head
+
+#         while fast and fast.next is not None:
+#             slow = slow.next
+#             fast = fast.next.next
+
+#         rev = self.reverse(slow)
+#         while rev:
+#             if head.val != rev.val:
+#                 return False
+#             head = head.next
+#             rev = rev.next
+        
+#         return True
+
+#     def reverse(self, head: ListNode) -> ListNode:
+#         prev = None
+#         curr = head
+
+#         while curr:
+#             temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = temp
+
+#         return prev
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         slow = head
         fast = head
 
-        while fast and fast.next is not None:
-            slow = slow.next
+        while fast and fast.next:
             fast = fast.next.next
+            slow = slow.next
 
-        rev = self.reverse(slow)
-        while rev:
-            if head.val != rev.val:
+        node = None
+        while slow:
+            temp = slow.next
+            slow.next = node
+            node = slow
+            slow = temp
+        
+        first, second = head, node
+
+        while second:
+            if first.val != second.val:
                 return False
-            head = head.next
-            rev = rev.next
+            
+            first = first.next
+            second = second.next
         
         return True
-
-    def reverse(self, head: ListNode) -> ListNode:
-        prev = None
-        curr = head
-
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-
-        return prev
-
-
