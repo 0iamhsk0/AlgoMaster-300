46cb8f27857ac44c2bee6feaec67ae2da5d74943
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-03 18:50:43 +0530

    Time: 56 ms (7.12%) | Memory: 20 MB (11.11%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -5,35 +5,35 @@
 #         self.next = None
 
 # Approach 1: Using HashSet, not space efficient
-# class Solution:
-#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         hash_set = set()
-#         while head:
-#             if head in hash_set:
-#                 return head
-#             hash_set.add(head)
-#             head = head.next
-      
-#         return None
-
-# Approach 2: Using Fast and Slow Pointers
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = head
-        fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                break
-        else:
-            return None 
+        hash_set = set()
+        while head:
+            if head in hash_set:
+                return head
+            hash_set.add(head)
+            head = head.next
+      
+        return None
 
-        fast = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
+# Approach 2: Using Fast and Slow Pointers
+# class Solution:
+#     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         slow = head
+#         fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#             if slow == fast:
+#                 break
+#         else:
+#             return None 
 
-        return slow  
+#         fast = head
+#         while slow != fast:
+#             slow = slow.next
+#             fast = fast.next
+
+#         return slow  
         
             
