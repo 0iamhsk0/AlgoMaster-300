472c2bec866005caca68c37ef9171d491bb3268c
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-07 18:51:53 +0530

    Create 2087-ConfirmationRate

---
+++ 2087-ConfirmationRate/2087-ConfirmationRate.sql
@@ -0,0 +1,9 @@
+-- Last updated: 10/7/2025, 6:51:49 PM
+-- Write your PostgreSQL query statement below
+SELECT s.user_id , 
+ROUND(COUNT(c.action) FILTER (WHERE c.action = 'confirmed')/COUNT(*)::decimal,2)
+AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
