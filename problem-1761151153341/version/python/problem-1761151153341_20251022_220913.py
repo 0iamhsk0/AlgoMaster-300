5541c1ee8b1e54fb27b8efe1a6a8fd75df2fcce4
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-22 22:17:42 +0530

    Delete problem-1761151153341 directory

--- problem-1761151153341/version/python/problem-1761151153341_20251022_220913.py
+++
@@ -1,7 +0,0 @@
-# Last updated: 10/22/2025, 10:09:13 PM
-from collections import Counter
-
-class Solution:
-    def maxNumberOfBalloons(self, text: str) -> int:
-        count = Counter(text)
-        return min(count['b'], count['a'], count['l'] // 2, count['o'] // 2, count['n'])
