5dfa58847f1b2f81a15a07d81adaa5f5e5e7c818
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-22 22:17:10 +0530

    Create 1189-MaximumNumberofBalloons

---
+++ 1189-MaximumNumberofBalloons/version/python/1189-MaximumNumberofBalloons_20251022_221709.py
@@ -0,0 +1,7 @@
+# Last updated: 10/22/2025, 10:17:09 PM
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = Counter(text)
+        return min(count['b'], count['a'], count['l'] // 2, count['o'] // 2, count['n'])
