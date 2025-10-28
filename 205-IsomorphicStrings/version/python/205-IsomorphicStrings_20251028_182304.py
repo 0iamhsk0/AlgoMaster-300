bccdcd72be725f2abbf42222740cac7a79be762d
Hemanth Sai Kumar.P <116539604+0iamhsk0@users.noreply.github.com>
2025-10-28 18:23:06 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/python/205-IsomorphicStrings_20251028_182304.py
@@ -0,0 +1,36 @@
+# Last updated: 10/28/2025, 6:23:04 PM
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # Approach - 1: Two hashmaps
+        # hash_map_s = dict()
+        # hash_map_t = {}
+
+        # for i in range(len(s)):
+        #     if s[i] not in hash_map_s:
+        #         hash_map_s[s[i]] = i
+
+        #     if t[i] not in hash_map_t:
+        #         hash_map_t[t[i]] = i
+
+        #     if hash_map_s[s[i]] != hash_map_t[t[i]]:
+        #         return False
+        # return True
+
+        ## Approach - 2: One hashmap
+        hash_map = {}
+
+        for i, j in zip(s, t):
+            if i in hash_map:
+                if hash_map[i] != j:
+                    return False
+
+            elif j in hash_map.values():
+                return False
+            
+            hash_map[i] = j
+        
+        return True
+            
+
+
+
